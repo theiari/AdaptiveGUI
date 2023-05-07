1af73d059aaa9d3a99bed282996f2eb2265cac17
theiari <72558865+theiari@users.noreply.github.com>
2023-05-07 17:28:54 +0200

    Updated to include more features
    - User can now load .txt files' content from the same directory into a textbox
       - Loaded files' name are shown on the right side of the interface
    - User can now save a .txt file, naming it arbitrary 
       - Saving a file with the same name of one already existing, will halt the process, leaving the older one intact.
    
    known issue:
    - When saving a new .txt file, a newline << \n >> is saved, causing to have lots of newline spaces when the files are loaded.

--- Adaptive.py
+++ Adaptive.py
@@ -3,6 +3,9 @@ import tkinter as tk
 from TestPageProva import TestPage
 from tkinter import filedialog
 from tkinter import Text
+from tkinter import END
+import tkinter.messagebox as msgbox
+import os
 #import TestPage
 
 LARGEFONT =("Verdana", 24)
@@ -64,10 +67,12 @@ class tkinterApp(tk.Tk):
   
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
+
+        
         tk.Frame.__init__(self, parent)
          
         # label of frame Layout 2
-        label = ttk.Label(self, text ="Adaptive 0.0.1", font = LARGEFONT)
+        label = ttk.Label(self, text ="Adaptive 0.1", font = LARGEFONT)
          
         # putting the grid in its place by using
         # grid
@@ -121,7 +126,7 @@ class StartPage(tk.Frame):
 
         button4 = ttk.Button(self, text ="paginaTest", # paginaTest is a class written in another file, used just for testing outer invocation (not working...)
         command = lambda : controller.show_frame(TestPage))
-        #button4.grid(row=7, column = 1) not placed for now, button4 is indeed debug stuff
+        #button4.grid(row=7, column = 1) #not placed for now, button4 is indeed debug stuff
 
         
             
@@ -132,8 +137,57 @@ class InstancePlanningPage(tk.Frame):
 
     
     def __init__(self, parent, controller):
-         
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent) 
+        def reset():
+            inputtxt.delete(1.0, END)
+            loadedFileLabel.config(text = "here is the list of all loaded files: \n")
+        
+        def loadFile() :
+             file_list = [file for file in os.listdir() if file.endswith(".txt")] #this is the list of all .txt file in this directory
+             
+
+             for file in file_list:
+                tempText = loadedFileLabel.cget("text")
+                loadedFileLabel.config( text= tempText +' '+str(file)+ '\n' )
+                with open(file, "r") as file:
+                    content = file.read()
+                #inputtxt.insert(END, '\n BEGIN OF FILE: '+file.name +'\n') this can be both useful and annoying
+                inputtxt.insert(END, content)
+                inputtxt.insert(END, '\n\n')
+             #if len(file_list) != 0:
+                 #we need to change a label
+       
+        def preSave():
+                info_window = tk.Toplevel()
+                info_window.grab_set()
+                info_window.title("Save the model")
+                info_window.geometry("200x130")
+                info_label = tk.Label(info_window, text="Please, select a name for the file")
+                info_label.pack(pady=10)
+                saveTextBox= tk.Text(info_window, height= 1, width= 12)
+                saveTextBox.pack(pady=10)
+                close_button = tk.Button(info_window, text="Save", command= lambda : saveFile(info_window, saveTextBox.get(0.0,END).strip() + ".txt") )
+                close_button.pack(pady=10)
+               
+
+        def saveFile(info_window, filename):
+            info_window.destroy()
+            info_window.update()
+
+            try:
+           
+                text_file = open(filename, "x") #the file will be saved as a .txt file, this will need to be changed depending on file extension type
+                text_file.write(inputtxt.get(1.0, END)) #the file is saved in the current folder
+                text_file.close()
+                inputtxt.delete(0.0, END) #textbox is now blank
+           #info_window = tk.Toplevel()
+                info_window = msgbox.showinfo("Information", "file " +filename+" has been saved!")
+            except FileExistsError:
+                msgbox.showerror("Error", "A file with this name already exist!")
+            
+
+
+        
         
       
         
@@ -158,31 +212,35 @@ class InstancePlanningPage(tk.Frame):
 
         #LABEL & BUTTON DECLARATION & GRID 
  
-        resetButton = ttk.Button(bottomFrame, text="RESET", command= '' ) #function to redet the whole design window, has not be implemented yet // TODO
+        resetButton = ttk.Button(bottomFrame, text="RESET", command= reset ) #function to redet the whole design window, has not be implemented yet // TODO
         resetButton.grid(column=0, row=0, padx = 3, pady = 3)
  
  
         #button MODEL used to retrieve .tdl or .sdl files
-        modelButton = ttk.Button(bottomFrame, text="MODEL", command= loadFile) #function to load a file from local storage, for now just a single one
+        modelButton = ttk.Button(bottomFrame, text="MODEL", command= preSave) #function to load a file from local storage, for now just a single one
         modelButton.grid(column=1, row=0, padx = 3, pady = 3)
         
         label = ttk.Label(centerFrame, text ="Instance Planning Design Time", font = LARGEFONT)
         label.grid(row = 0, column = 0)
 
+        
+        loadedFileLabel = ttk.Label(rightFrame, text='here is the list of all loaded files: \n')
+        loadedFileLabel.grid(row = 1, column= 5, padx= 10)
+
         startPageButton = ttk.Button(bottomFrame, text ="Home",
                             command = lambda : controller.show_frame(StartPage))   
         startPageButton.grid(row = 0, column = 2, padx = 3, pady = 3)
 
-        modelServiceButton = ttk.Button(topFrame, text = " Model service ")# no action, WIP
+        modelServiceButton = ttk.Button(topFrame, text = " Service Template ", command= loadFile)
         modelServiceButton.grid(row = 1, column = 0)
 
-        modelTargetButton =  ttk.Button(topFrame, text = " Model target ")# no action, WIP
+        modelTargetButton =  ttk.Button(topFrame, text = " Target Template ", command= loadFile)
         modelTargetButton.grid(row = 1, column = 1)
 
 
         inputtxt = tk.Text(centerFrame,
-                height = 10,
-                width = 40,
+                height = 14,
+                width = 60,
                 )
         inputtxt.grid(row = 1 , column= 0 )
         global dynamicLabel 
@@ -190,15 +248,12 @@ class InstancePlanningPage(tk.Frame):
         dynamicLabel.grid(row = 2, column= 6)
 
 
-    def reset():
-        inputtxt = ''
-        file = ''
-
-
+   
 # second window frame page1  --INSTANCE PLANNING RUNTIME TEST --
 class RunTimePage(tk.Frame):
      
     def __init__(self, parent, controller):
+        
          
         tk.Frame.__init__(self, parent)
         label = ttk.Label(self, text ="Instance planning RUNTIME", font = LARGEFONT)
@@ -216,9 +271,56 @@ class RunTimePage(tk.Frame):
 # second window frame page2  --STOCHASTIC POLICY--
 class StochasticPolicyPage(tk.Frame):
     def __init__(self, parent, controller):
-         
-        tk.Frame.__init__(self, parent)
         
+        tk.Frame.__init__(self, parent)
+
+        def reset():
+            inputtxt.delete(1.0, END)
+            loadedFileLabel.config(text = "here is the list of all loaded files: \n")
+        
+        def loadFile() :
+             file_list = [file for file in os.listdir() if file.endswith(".txt")] #this is the list of all .txt file in this directory
+             
+
+             for file in file_list:
+                tempText = loadedFileLabel.cget("text")
+                loadedFileLabel.config( text= tempText +' '+str(file)+ '\n' )
+                with open(file, "r") as file:
+                    content = file.read()
+                #inputtxt.insert(END, '\n BEGIN OF FILE: '+file.name +'\n') this can be both useful and annoying
+                inputtxt.insert(END, content)
+                inputtxt.insert(END, '\n\n')
+             #if len(file_list) != 0:
+                 #we need to change a label
+       
+        def preSave():
+                info_window = tk.Toplevel()
+                info_window.grab_set()
+                info_window.title("Save the model")
+                info_window.geometry("200x130")
+                info_label = tk.Label(info_window, text="Please, select a name for the file")
+                info_label.pack(pady=10)
+                saveTextBox= tk.Text(info_window, height= 1, width= 12)
+                saveTextBox.pack(pady=10)
+                close_button = tk.Button(info_window, text="Save", command= lambda : saveFile(info_window, saveTextBox.get(0.0,END).strip() + ".txt") )
+                close_button.pack(pady=10)
+               
+
+        def saveFile(info_window, filename):
+            info_window.destroy()
+            info_window.update()
+
+            try:
+           
+                text_file = open(filename, "x") #the file will be saved as a .txt file, this will need to be changed depending on file extension type
+                text_file.write(inputtxt.get(1.0, END)) #the file is saved in the current folder
+                text_file.close()
+                inputtxt.delete(0.0, END) #textbox is now blank
+           #info_window = tk.Toplevel()
+                info_window = msgbox.showinfo("Information", "file " +filename+" has been saved!")
+            except FileExistsError:
+                msgbox.showerror("Error", "A file with this name already exist!")
+            
 
         # FRAME DECLARATION & GRID
 
@@ -235,38 +337,45 @@ class StochasticPolicyPage(tk.Frame):
         bottomFrame = tk.Frame(self, width = 200 , height= 200)
         bottomFrame.grid(row = 8, column= 0 )
 
-
+        inputtxt = tk.Text(centerFrame,
+                height = 15,
+                width = 60,
+                )
+        inputtxt.grid(row = 1 , column= 0 )
 
 
         #LABEL & BUTTON DECLARATION & GRID 
  
-        resetButton = ttk.Button(bottomFrame, text="RESET", command= '') #function to redet the whole design window, has not be implemented yet // TODO
+        resetButton = ttk.Button(bottomFrame, text="RESET", command= reset) #function to redet the whole design window, has not be implemented yet // TODO
         resetButton.grid(column=0, row=0, padx = 3, pady = 3)
- 
+    
+        
  
         #button MODEL used to retrieve .tdl or .sdl files
-        modelButton = ttk.Button(bottomFrame, text="MODEL", command= loadFile) #function to load a file from local storage, for now just a single one
+        modelButton = ttk.Button(bottomFrame, text="MODEL", command= preSave) #function to load a file from local storage, for now just a single one
         modelButton.grid(column=1, row=0, padx = 3, pady = 3)
         
         label = ttk.Label(centerFrame, text ="Stochastic Policy Design Time", font = LARGEFONT)
         label.grid(row = 0, column = 0)
 
+
+        
+        loadedFileLabel = ttk.Label(rightFrame, text='here is the list of all loaded files: \n')
+        loadedFileLabel.grid(row = 1, column= 5, padx= 10)
+
+
         startPageButton = ttk.Button(bottomFrame, text ="Home",
                             command = lambda : controller.show_frame(StartPage))   
         startPageButton.grid(row = 0, column = 2, padx = 3, pady = 3)
 
-        modelServiceButton = ttk.Button(topFrame, text = " Model service ")# no action, WIP
+        modelServiceButton = ttk.Button(topFrame, text = " Service Template ", command= loadFile )# no action, WIP
         modelServiceButton.grid(row = 1, column = 0)
 
-        modelTargetButton =  ttk.Button(topFrame, text = " Model target ")# no action, WIP
+        modelTargetButton =  ttk.Button(topFrame, text = " Target Template ",command= loadFile)# no action, WIP
         modelTargetButton.grid(row = 1, column = 1)
 
 
-        inputtxt = tk.Text(centerFrame,
-                height = 10,
-                width = 40,
-                )
-        inputtxt.grid(row = 1 , column= 0 )
+       
         global dynamicLabel 
         dynamicLabel = ttk.Label(rightFrame, text = "select some files" ) #this label shows the directory of the selected file
         dynamicLabel.grid(row = 2, column= 6)
@@ -278,6 +387,60 @@ class StochasticConstraintsBasedPolicy(tk.Frame):
          
         tk.Frame.__init__(self, parent)
         
+        
+        def reset():
+            inputtxt.delete(1.0, END)
+            loadedFileLabel.config(text = "here is the list of all loaded files: \n")
+        
+        def loadFile() :
+             file_list = [file for file in os.listdir() if file.endswith(".txt")] #this is the list of all .txt file in this directory
+             
+
+             for file in file_list:
+                tempText = loadedFileLabel.cget("text")
+                loadedFileLabel.config( text= tempText +' '+str(file)+ '\n' )
+                with open(file, "r") as file:
+                    content = file.read()
+                #inputtxt.insert(END, '\n BEGIN OF FILE: '+file.name +'\n') this can be both useful and annoying
+                inputtxt.insert(END, content)
+                inputtxt.insert(END, '\n\n')
+             #if len(file_list) != 0:
+                 #we need to change a label
+       
+        def preSave():
+                info_window = tk.Toplevel()
+                info_window.grab_set()
+                info_window.title("Save the model")
+                info_window.geometry("200x130")
+                info_label = tk.Label(info_window, text="Please, select a name for the file")
+                info_label.pack(pady=10)
+                saveTextBox= tk.Text(info_window, height= 1, width= 12)
+                saveTextBox.pack(pady=10)
+                close_button = tk.Button(info_window, text="Save", command= lambda : saveFile(info_window, saveTextBox.get(0.0,END).strip() + ".txt") )
+                close_button.pack(pady=10)
+        
+                
+        
+               
+
+        def saveFile(info_window, filename):
+            info_window.destroy()
+            info_window.update()
+
+            try:
+           
+                text_file = open(filename, "x") #the file will be saved as a .txt file, this will need to be changed depending on file extension type
+                text_file.write(inputtxt.get(1.0, END)) #the file is saved in the current folder
+                text_file.close()
+                inputtxt.delete(0.0, END) #textbox is now blank
+           #info_window = tk.Toplevel()
+                info_window = msgbox.showinfo("Information", "file " +filename+" has been saved!")
+            except FileExistsError:
+                msgbox.showerror("Error", "A file with this name already exist!")
+            
+           #info_window.title("Information")
+           #info_window.geometry("200x100")
+           
 
         # FRAME DECLARATION & GRID
 
@@ -299,37 +462,47 @@ class StochasticConstraintsBasedPolicy(tk.Frame):
 
         #LABEL & BUTTON DECLARATION & GRID 
  
-        resetButton = ttk.Button(bottomFrame, text="RESET", command= '' ) #function to redet the whole design window, has not be implemented yet // TODO
+        resetButton = ttk.Button(bottomFrame, text="RESET", command= reset ) #function to redet the whole design window, has not be implemented yet // TODO
         resetButton.grid(column=0, row=0, padx = 3, pady = 3)
  
- 
+        #model button should load the file
         #button MODEL used to retrieve .tdl or .sdl files
-        modelButton = ttk.Button(bottomFrame, text="MODEL", command= loadFile) #function to load a file from local storage, for now just a single one
+        modelButton = ttk.Button(bottomFrame, text="MODEL", command= preSave ) #function to load a file from local storage, for now just a single one
         modelButton.grid(column=1, row=0, padx = 3, pady = 3)
-        
+
+        modelButtonLoad= ttk.Button(bottomFrame, text="load", command= loadFile ) #not sure if this is essential, for now I'll just leave it here
+        modelButtonLoad.grid(column=5, row=0, padx = 3, pady = 3)               #it does the same action as Service/Target Template, for now.
+
         label = ttk.Label(centerFrame, text ="Stochastic Constraints-based Policy Design Time", font = LARGEFONT)
         label.grid(row = 0, column = 0)
 
+        loadedFileLabel = ttk.Label(rightFrame, text='here is the list of all loaded files: \n')
+        loadedFileLabel.grid(row = 1, column= 5, padx= 10)
         startPageButton = ttk.Button(bottomFrame, text ="Home",
                             command = lambda : controller.show_frame(StartPage))   
         startPageButton.grid(row = 0, column = 2, padx = 3, pady = 3)
 
-        modelServiceButton = ttk.Button(topFrame, text = " Model service ")# no action, WIP
+        modelServiceButton = ttk.Button(topFrame, text = " Service Template ", command= loadFile)# no action, WIP
         modelServiceButton.grid(row = 1, column = 0)
 
-        modelTargetButton =  ttk.Button(topFrame, text = " Model target ")# no action, WIP
+        modelTargetButton =  ttk.Button(topFrame, text = " Target Template ", command= loadFile)# no action, WIP
         modelTargetButton.grid(row = 1, column = 1)
 
 
         inputtxt = tk.Text(centerFrame,
-                height = 10,
-                width = 40,
+                height = 15,
+                width = 60,
                 )
         inputtxt.grid(row = 1 , column= 0 )
         global dynamicLabel 
         dynamicLabel = ttk.Label(rightFrame, text = "select some files" ) #this label shows the directory of the selected file
-        dynamicLabel.grid(row = 2, column= 6)
+        #dynamicLabel.grid(row = 2, column= 6)
 
+
+        
+
+
+#this one will be useful for the runtime section
 def loadFile(): #select the file from os explorer and return its directory as string type
     filetypes  = (
         ('text files', '*.txt'), # this is just for debug, eventually will be deleted
@@ -339,7 +512,7 @@ def loadFile(): #select the file from os explorer and return its directory as st
     global file
     file = filedialog.askopenfilename(
         title='Select the file', #name of the tab
-        initialdir='/', #initial shown directory
+        initialdir="C:/Users/anton/OneDrive/Documenti/Software Engineering/Fellowship/Adaptive", #initial shown directory
         filetypes= filetypes) #filters types of file can be selected
 
     dynamicLabel.config(text= file )
@@ -350,10 +523,6 @@ def loadFile(): #select the file from os explorer and return its directory as st
      #   )
 
     return file
-
-    
-
-
 
 
 # Driver Code
